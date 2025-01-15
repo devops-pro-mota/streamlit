1c625e3e6e17ae0ab1f71d9486d127eead920357
devops-pro-mota <151980603+devops-pro-mota@users.noreply.github.com>
2025-01-15 01:05:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import requests
-
+###
+# Função para aplicar estilos CSS personalizados
+def adicionar_estilos_css():
+    st.markdown(
+        """
+        <style>
+        .reportview-container .main .block-container{
+            font-family: 'Verdana';
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 def fazear_pergunta_llm(pergunta, chave_api):
-    url = 'http://localhost/api/v1/workspace/documentos/chat'
+    url = 'http://192.168.0.88:3001/api/v1/workspace/documentos/chat'
     headers = {
         'Content-Type': 'application/json',
         'accept': 'application/json',
@@ -17,26 +29,14 @@ def fazear_pergunta_llm(pergunta, chave_api):
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
-        # Inspecionar conteúdo da resposta
-        conteudo_resposta_bruto = response.content
-        st.write(f"Resposta bruta: {conteudo_resposta_bruto}")
-
-        try:
-            # Tentar decodificar como UTF-8
-            conteudo_resposta = conteudo_resposta_bruto.decode('utf-8')
-        except UnicodeDecodeError as e:
-            st.write(f"Erro de decodificação UTF-8: {e}")
-            # Decodificar usando latin-1 como fallback
-            conteudo_resposta = conteudo_resposta_bruto.decode('latin-1')
-
         conteudo_json = response.json()
         return conteudo_json.get('textResponse', 'Resposta não encontrada')
     else:
         return f"Erro ao fazer a pergunta {response.status_code}"
 
-st.title('Unimed Assistent!')
+st.title('UnimedBR Assistent!')
 chave_api = "B2VDWKK-C6VMS23-P6WYGCK-XPEQE7Q"
-
+adicionar_estilos_css()
 pergunta_usuario = st.text_input('Faça sua pergunta:')
 
 if st.button('Enviar'):
